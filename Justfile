07f08d93d470792376b4607a2768f6d48af4ccaa
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-10-06 15:22:52 +0200

    Added the ability run a specific file.

--- Justfile
+++ Justfile
@@ -1,8 +1,9 @@
 grammar:
     bnfc "src/Lang.bnfc" -o "src" -m -d --functor && cd src && make
 
-run:
-    just grammar && cabal run
-
+runTest: 
+    just grammar && cabal run CoolLang -- test/test.cl
+run file: 
+    just grammar && cabal run CoolLang -- {{file}}
 ngrammar:
     nix-shell --run "just grammar"
--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,18 @@
 module Main where
 
-import           Lang.Par   (myLexer, pExpr)
-import           Lang.Print (printTree)
-import           System.IO  (IOMode (ReadMode), hGetContents, openFile)
+import           Control.Monad      (when)
+import           Lang.Par           (myLexer, pExpr)
+import           Lang.Print         (printTree)
+import           System.Environment (getArgs)
+import           System.IO          (IOMode (ReadMode), hGetContents, openFile)
 
 main :: IO ()
 main = do
-    file <- openFile "test/test.cl" ReadMode
+    args <- getArgs
+    when (null args) (error "No file supplied!")
+    let filePath = head args
+
+    file <- openFile filePath ReadMode
     fileContent <- hGetContents file
     case pExpr . myLexer $ fileContent of
         Left ok -> putStrLn $ printTree ok
